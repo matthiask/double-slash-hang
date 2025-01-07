a323c951a685d5ea49f84855a0f85ee68935dc74
Matthias Kestenholz <mk@feinheit.ch>
2025-01-07 13:22:30 +0100

    Commit

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,6 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import rspack from "@rspack/core"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
@@ -18,16 +19,33 @@ const config = {
   entry: {
     main: "./src/index",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        type: "javascript/auto",
+        use: [
+          { loader: rspack.CssExtractRspackPlugin.loader },
+          { loader: "css-loader" },
+        ],
+      },
+      {
+        test: /\.(png|woff2?|svg|eot|ttf|otf|gif|jpe?g|mp3|wav)$/i,
+        type: "asset",
+        parser: { dataUrlCondition: { maxSize: 512 /* bytes */ } },
+      },
+    ]
+  },
+  plugins: [
+    new rspack.CssExtractRspackPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
   output: {
     clean: true,
     path: isRunningWebpack
       ? path.resolve(__dirname, "webpack-dist")
       : path.resolve(__dirname, "rspack-dist"),
     filename: "[name].js",
-  },
-  experiments: {
-    css: true,
   },
 };
 
